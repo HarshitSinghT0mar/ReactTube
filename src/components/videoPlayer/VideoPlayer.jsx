0b4179ef2b456737dd2a17463fd05d49f4e0f186
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2024-02-19 21:08:59 +0530

    refactor: remove isHovering state

--- src/components/videoPlayer/VideoPlayer.jsx
+++ src/components/videoPlayer/VideoPlayer.jsx
@@ -7,7 +7,7 @@ const VideoPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
-  const [isHovering, setIsHovering] = useState(false);
+
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [playbackSpeed, setPlaybackspeed] = useState(1);
   const [volume, setVolume] = useState(1);
@@ -45,10 +45,6 @@ const VideoPlayer = () => {
     videoRef.current.playbackRate = playbackSpeed;
   }, [selectedVideo]);
 
-  const handleTimeUpdate = () => {
-    setCurrentTime(videoRef.current.currentTime);
-  };
-
   const handleProgressBarClick = (event) => {
     event.stopPropagation();
     const progressBarRect = event.currentTarget.getBoundingClientRect();
@@ -74,12 +70,17 @@ const VideoPlayer = () => {
       setIsPlaying(false);
     }
   };
+  const handleKeyUp = (e) => {
+    if (e.code === "space") {
+      togglePlayPause();
+    }
+  };
 
   return (
     <div
-      className="rounded-lg relative w-full bg-black py-8"
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
+      className="rounded-lg relative w-full group outline-none border-none bg-black py-8"
+      onKeyUp={handleKeyUp}
+      tabIndex={0}
     >
       <video
         ref={videoRef}
@@ -87,7 +88,7 @@ const VideoPlayer = () => {
         poster={selectedVideo?.thumb}
         className="w-full shadow-lg h-full object-contain"
         onClick={togglePlayPause}
-        onTimeUpdate={handleTimeUpdate}
+        onTimeUpdate={() => setCurrentTime(videoRef.current.currentTime)}
         onLoadedMetadata={() => setDuration(videoRef.current.duration)}
         onEnded={handleVideoEnd}
         controls={false}
@@ -96,9 +97,7 @@ const VideoPlayer = () => {
         Your browser does not support the video tag.
       </video>
       <div
-        className={`absolute left-0 right-0 bottom-8 z-10 bg-gray-200 h-2 cursor-pointer ${
-          isHovering ? "opacity-100" : "opacity-0"
-        }`}
+        className={`absolute hidden group-hover:block left-0 right-0 bottom-8 z-10 bg-gray-200 h-2 cursor-pointer `}
         onClick={handleProgressBarClick}
       >
         <div
@@ -106,33 +105,32 @@ const VideoPlayer = () => {
           style={{ width: `${(currentTime / duration) * 100}%` }}
         ></div>
       </div>
-
-      {isHovering && (
-        <button
-          className="absolute inset-0 w-full h-full flex items-center justify-center"
-          onClick={togglePlayPause}
-        >
-          {!videoRef.current.paused || isPlaying ? (
-            <svg
-              className="w-16 h-16 text-white"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M6 6h4v12H6zm8 0h4v12h-4z" />
-            </svg>
-          ) : (
-            <svg
-              className="w-16 h-16 text-white"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M8 5v14l11-7z" />
-            </svg>
-          )}
-        </button>
-      )}
+      (
+      <button
+        className="absolute hidden group-hover:flex inset-0 w-full h-full items-center justify-center"
+        onClick={togglePlayPause}
+      >
+        {!videoRef.current?.paused || isPlaying ? (
+          <svg
+            className="w-16 h-16 text-white "
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M6 6h4v12H6zm8 0h4v12h-4z" />
+          </svg>
+        ) : (
+          <svg
+            className="w-16 h-16 text-white"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M8 5v14l11-7z" />
+          </svg>
+        )}
+      </button>
+      )
       <div className="flex justify-between items-center my-1 px-4 absolute bottom-1 w-full">
         <div className="flex justify-between w-full">
           <span className="text-white text-sm">{`${formatTime(
@@ -152,14 +150,16 @@ const VideoPlayer = () => {
               setVolume(newVolume);
               videoRef.current.volume = newVolume;
             }}
-            className="w-24 h-4 bg-gray-800 rounded-full overflow-hidden appearance-none slider"
+            className="w-24 h-4 outline-none bg-gray-800 rounded-full overflow-hidden appearance-none slider"
             style={{
               background: `linear-gradient(to right, #4F2485 ${
                 volume * 100
               }%, #ccc ${volume * 100}%)`,
             }}
           />
-          <label className="text-white whitespace-nowrap" >Playback Speed:</label>
+          <label className="text-white whitespace-nowrap">
+            Playback Speed:
+          </label>
           <select
             className=" bg-gray-800 border text-xs border-gray-700 text-white rounded-sm focus:outline-none"
             value={playbackSpeed}
